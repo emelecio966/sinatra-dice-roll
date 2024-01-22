aae09e11f5556cd4d6a9565f6c48b66a41486e3d
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-01-22 15:36:34 +0000

    get dice

--- dice.rb
+++ dice.rb
@@ -25,3 +25,27 @@ get("/dice/2/6") do
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
+
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+  second_die = rand(1..20)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+end
+#GET /dice/5/4 
