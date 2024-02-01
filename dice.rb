24f2e77ae97849f1a824a0d2f3a50bdbb9470be6
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-02-01 15:16:58 +0000

    outcome

--- dice.rb
+++ dice.rb
@@ -19,7 +19,7 @@ end
 get("/dice/2/6") do
   first_die6 = rand(1..6)
   second_die6 = rand(1..6)
-  sum = first_die6 + second_die6
+  sum6 = first_die6 + second_die6
 	
   @outcome6 = "You rolled a #{first_die6} and a #{second_die6} for a total of #{sum6}."
 	
@@ -30,7 +30,7 @@ end
 get("/dice/2/10") do
   first_die10 = rand(1..10)
   second_die10 = rand(1..10)
-  sum = first_die10 + second_die10
+  sum10 = first_die10 + second_die10
 	
   @outcome10 = "You rolled a #{first_die10} and a #{second_die10} for a total of #{sum10}."
 	
@@ -40,7 +40,7 @@ end
 get("/dice/1/20") do
   first_die20 = rand(1..20)
   second_die20 = rand(1..20)
-  sum = first_die20 + second_die20
+  sum20 = first_die20 + second_die20
 	
   @outcome20 = "You rolled a #{first_die20} and a #{second_die20} for a total of #{sum20}."
 	
@@ -53,7 +53,7 @@ get("/dice/5/4") do
   third_die4 = rand(1..5)
   four_die4 = rand(1..5)
   five_die4 = rand(1..5) 
-  sum = first_die4 + second_die4 + third_die4 + four_die4 + five_die4
+  sum4 = first_die4 + second_die4 + third_die4 + four_die4 + five_die4
 	
   @outcome4 = "You rolled a #{first_die4}, #{second_die4}, #{third_die4}, #{four_die4} and a #{five_die4} for a total of #{sum4}."
 	
